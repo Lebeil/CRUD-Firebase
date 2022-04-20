138b0b9eb4a157620d1d4d132f407efa80248b44
Liedel <liedel75@gmail.com>
2022-04-20 10:06:05 +0200

    add loggin

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import '../src/styles/index.scss'
 import ConnectModal from './components/ConnectModal';
 import {useState} from "react";
-import { onAuthStateChanged } from "firebase/auth"
+import {onAuthStateChanged, signOut} from "firebase/auth"
 import {auth} from "./utils/firebase.config";
 import CreatePost from "./components/CreatePost";
 
 function App() {
-  const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null)
 
-  onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-  })
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    })
 
-  return (
-    <div className="App">
-      <div className='app-header'>
-          {user && (
-            <div className="user-infos">
-                <span>{user?.displayName[0]}</span>
-                <h4>{user?.displayName}</h4>
-                <button>
-                    <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                </button>
+    const handleLogout = async () => {
+        await signOut(auth);
+    }
+
+    return (
+        <div className="App">
+            <div className='app-header'>
+                {user && (
+                    <div className="user-infos">
+                        <span>{user?.displayName[0]}</span>
+                        <h4>{user?.displayName}</h4>
+                        <button onClick={()=> handleLogout()}>
+                            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                        </button>
+                    </div>
+                )}
+                {user ? <CreatePost/> : <ConnectModal/>}
             </div>
-          )}
-          {user ? <CreatePost/> : <ConnectModal/>}
-      </div>
-      <div className='posts-container'>
+            <div className='posts-container'>
 
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default App;
